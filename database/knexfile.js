726addc21d28b86080be91c6e54df5662f620dc8
abdulbasit-dev <abdullbasit99salah@gmail.com>
2021-11-07 09:36:46 +0300

    set up database

---
+++ database/knexfile.js
@@ -0,0 +1,21 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: process.env.DB_CONNECTION,
+    connection: {
+      host: process.env.DB_HOST,
+      port:process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database:process.env.DB_DATABASE,
+    },
+    pool: {
+      min: 0,
+      max: 7,
+    },
+    migrations: {
+      tableName: 'migrations'
+    }
+  }
+};
