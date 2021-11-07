e165d202b0e4f357982be7b5df8c5de1b6345439
abdulbasit-dev <abdullbasit99salah@gmail.com>
2021-11-07 10:08:49 +0300

    set up error handler

--- controllers/test.js
+++ controllers/test.js
@@ -1,4 +1,5 @@
 const db = require('../database/conection')
+const HttpError = require('../utils/httpError');
 
 const testFunction= (req,res)=>{
   return res.json('hello from test route and test controller')
