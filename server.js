3e5e2701c7a7beb1c5709de93910a2f5c6bf9685
abdulbasit-dev <abdullbasit99salah@gmail.com>
2021-11-07 09:54:22 +0300

    set up server file

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+//create app
+const app = express();
+const port = process.env.PORT || PORT
+app.use(bodyParser.json())
+
+// COSR handler
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  next();
+});
+
+//ROUETS
+app.get('/',(req,res)=>{
+  return res.json(["hello from nodejs server"])
+})
+
+app.listen(port, ()=>console.log(`server run on port ${port}`))
